01b2c5bfabcf9cddb36b8cccaea843e94f1787fd
Rodrigo González <rodrigoalexissss@gmail.com>
2024-06-30 18:48:45 -0400

    Explicación M3

---
+++ m3/class/database.php
@@ -0,0 +1,17 @@
+<?php
+/* @autor Rodrigo González */
+
+class Database{
+  private $connection;
+
+  function __construct($base_datos, $host, $user, $pass) {
+    $conection = "mysql:dbname=".$base_datos.";host=$host";
+    $this->connection = new PDO($conection, $user, $pass);
+    if (!$this->connection) throw new Exception("No se ha podido realizar la conexión");
+  }
+  
+  public function getConnection(){
+    return $this->connection;
+  }
+}
+?>
